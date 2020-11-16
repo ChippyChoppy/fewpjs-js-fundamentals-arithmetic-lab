af6862d05eb8b2c451799a51da72f3f7af52ff2d
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-11-16 00:47:26 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// Write your code he
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2 
 
-// Write your code here
+const random = Math.random()
+
+const mod = 24 % 5
+
+const max = (1, 3, 7, 20)
